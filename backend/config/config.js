c30635c367be2284ffd1843751cacd3d4964f391
devsathwara-enacton <devsath.enacton@gmail.com>
2024-01-22 09:33:52 +0530

    Initial commit

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,16 +1,16 @@
-const dotenv=require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
- const config={
-env:{
-    app:{
-        port:process.env.PORT,
-        dburl:process.env.DB_URL,
-        jwtSecretKey: process.env.SECRET,
-        expiresIn: process.env.EXPIRESIN,
-        cookieExpiration: new Date(Date.now() + 24 * 60 * 60 * 1000)
-    }
-}
- }
+const config = {
+  env: {
+    app: {
+      port: process.env.PORT,
+      dbUrl: process.env.DB_URL,
+      jwtSecretKey: process.env.SECRET,
+      expiresIn: process.env.EXPIRESIN,
+      cookieExpiration: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    },
+  },
+};
 
- module.exports=config;
+module.exports = config;
